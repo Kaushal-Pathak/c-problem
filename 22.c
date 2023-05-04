154d294f9442ec09aeca31886920337553782638
Kaushal-Pathak <132580286+Kaushal-Pathak@users.noreply.github.com>
2023-05-04 23:59:07 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i, size;
-    printf("enter number of elements of array\n");
+    printf("enter elements of array\n");
     scanf("%d", &size);
     int arr[size];
     printf("Enter the elements: ");
@@ -10,7 +10,7 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-    printf("Odd number of array are\n");
+    printf("Odd number are\n");
     for (i = 0; i < size; i++){
         if(arr[i]%2!=0){
             printf("%d \t",arr[i]);
