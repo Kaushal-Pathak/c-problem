57f21b5e67c4afcbb6fd87f9f8c83c1c95c30467
Kaushal-Pathak <132580286+Kaushal-Pathak@users.noreply.github.com>
2023-05-04 23:57:24 +0530

    Update 23.c

--- 23.c
+++ 23.c
@@ -6,7 +6,7 @@ int main(){
     int size1=sizeof(arr1)/sizeof(arr1[0]);
     int size2=sizeof(arr2)/sizeof(arr2[0]);
     if(size1 == size2){
-        printf("size of both arrays are equal");
+        printf("size of arrays are equal");
     }else{
         printf("size of arrays are not equal");
     }
